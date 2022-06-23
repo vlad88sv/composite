74cccaf454fb94116a41ce78690ef820dde789eb
Vladimir Hidalgo <vladimiroski@gmail.com>
2022-06-23 09:39:46 -0600

    Composite part 3

--- app/Http/Controllers/Composite.php
+++ app/Http/Controllers/Composite.php
@@ -12,7 +12,7 @@ class Composite extends Controller
     
         $img2 = new \Imagick();
         $img2->readImage(realpath(resource_path('img/logo.jpg')));
-    
+
         $img1->resizeimage(
             $img2->getImageWidth(),
             $img2->getImageHeight(),
@@ -24,12 +24,14 @@ class Composite extends Controller
         $opacity->newPseudoImage(
             $img1->getImageHeight(), 
             $img1->getImageWidth(),
-            "gradient:gray(10%)-gray(90%)"
+            "gradient:gray(20%)-gray(100%)"
         );
-        //$opacity->rotateimage('black', 95);
-    
+        //$opacity->rotateimage('black', 99);
+        $img2->scaleImage(60, 60);
+
         $img2->compositeImage($opacity, \Imagick::COMPOSITE_COPYOPACITY, 0, 0);
-        $img1->compositeImage($img2, \Imagick::COMPOSITE_ATOP, 0, 0);
+        $img1->compositeImage($img2, \Imagick::COMPOSITE_ATOP, 85, 170);
+        $img1->compositeImage($img2, \Imagick::COMPOSITE_ATOP, 260, 170);
     
         header("Content-Type: image/jpeg");
         echo $img1->getImageBlob();
